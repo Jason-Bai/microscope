7a767c1d4d6d85be499d5c04bc3842d317f6b9c3
jason <boybai1213@gmail.com>
2014-06-24 23:18:26 +0800

    added the same url not insert

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -7,8 +7,16 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val(),
             message: $(e.target).find('[name=message]').val()
         };
-
+        /*
         post._id = Posts.insert(post);
         Router.go('postPage', post);
+        */
+        Meteor.call('post', post, function (err, id) {
+            if(err) {
+                return alert(err.reason);
+            }
+
+            Router.go('postPage', {_id: id});
+        });
     }
 });
